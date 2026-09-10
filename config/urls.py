6a00f8e452dcd09e1e4dce98425d5b3906a79c92
Okezie Elvis Ugochukwu <etzelvisbrightfuture@gmail.com>
2026-09-10 17:26:05 +0100

    Clean up urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from django.core.management import call_command
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # TEMPORARY: Run migrations on Render
-    path('run-migrations-now/', lambda request: HttpResponse(str(call_command('migrate')))),
-    
-    # Your App URLs
     path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
     path('investments/', include('investments.urls')),
