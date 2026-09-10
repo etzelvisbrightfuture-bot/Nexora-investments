be08113a2a0533b96daf0c7df85e26b09d10b262
Okezie Elvis Ugochukwu <etzelvisbrightfuture@gmail.com>
2026-09-10 17:47:58 +0100

    Add force build database URL

--- config/urls.py
+++ config/urls.py
@@ -2,19 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.core.management import call_command
+from django.db import connection
 from io import StringIO
 
-# This function will run your migrations and show you the result
-def trigger_migrations(request):
+# This function forces Django to rebuild the missing tables
+def force_build_database(request):
+    # 1. Clear the migration history so Django is forced to build the tables again
+    with connection.cursor() as cursor:
+        cursor.execute("DELETE FROM django_migrations WHERE app='investments';")
+    
+    # 2. Run the migrations to actually create the tables
     out = StringIO()
-    call_command('migrate', stdout=out)
+    call_command('migrate', 'investments', stdout=out)
+    
     return HttpResponse(f"<pre>{out.getvalue()}</pre>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # THE MAGIC URL
-    path('run-migrations-now/', trigger_migrations),
+    # THE MAGIC FIX URL
+    path('force-build-db/', force_build_database),
     
     path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
