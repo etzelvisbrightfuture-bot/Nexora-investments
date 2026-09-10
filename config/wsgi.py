2be46f12d263e38852123ec9712e96aa959a33c6
Okezie Elvis Ugochukwu <etzelvisbrightfuture@gmail.com>
2026-09-10 01:58:16 +0100

    Fix static files and WhiteNoise configuration for production

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+# Wrap the application with WhiteNoise to serve static files
+application = WhiteNoise(application, root='staticfiles/')
