c9448e82fd069e22e7a4c6b116bccdc0fc5792dd
Okezie Elvis Ugochukwu <etzelvisbrightfuture@gmail.com>
2026-09-10 13:00:33 +0100

    Add temporary migration URL

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,16 @@
-from django.http import HttpResponse
-from django.core.management import call_command
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.core.management import call_command
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # TEMPORARY: Run migrations on Render
+    path('run-migrations-now/', lambda request: HttpResponse(str(call_command('migrate')))),
+    
+    # Your App URLs
     path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
     path('investments/', include('investments.urls')),
-    path('run-migrations-now/', lambda request: HttpResponse(str(call_command('migrate')))),
 ]
-
-# Add this at the bottom to serve images in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
